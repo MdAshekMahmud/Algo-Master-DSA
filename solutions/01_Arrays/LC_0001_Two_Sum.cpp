db061916853e419bad756909c12a6e15ee2366e3
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-07-07 01:34:58 +0600

    Solve Two Sum

--- solutions/01_Arrays/LC_0001_Two_Sum.cpp
+++ solutions/01_Arrays/LC_0001_Two_Sum.cpp
@@ -2,19 +2,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int majorityElement(vector<int> &nums) {
+    int majorityElement(vector<int> &nums)
+    {
         int n = nums.size();
 
         unordered_map<int, int> mp;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             mp[nums[i]]++;
         }
         int count = 0;
-        for (auto el : mp) {
-            if (el.second > n / 2) {
+        for (auto el : mp)
+        {
+            if (el.second > n / 2)
+            {
                 return el.first;
             }
         }
@@ -23,7 +28,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
